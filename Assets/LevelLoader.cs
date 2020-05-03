0d93cc1ecddecb49f8b5bcde8b129d623b95f5db
catngocd <catngocnd@gmail.com>
2020-05-02 22:51:04 -0400

    Add all files

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }        
+    }
+
+    public void LoadNextLevel()
+    {
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        // Play animation
+        transition.SetTrigger("Start");
+        // Wait
+        yield return new WaitForSeconds(1);
+        // Load scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
