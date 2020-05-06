38f2f9b20b81098ab2df4d60851b007a5514d12e
catngocd <catngocnd@gmail.com>
2020-05-06 18:50:19 -0400

    Messenger screen added

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public GameObject Panel;
+
+    // Start is called before the first frame update
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            if(Panel != null) {
+                bool isActive = Panel.activeSelf;
+                if (isActive) {
+                    Panel.SetActive(false);
+                }
+            } 
+        }         
+    }
+}
---
+++ Assets/Scripts/Messenger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Messenger : MonoBehaviour
+{
+
+    public void OpenMessenger() {
+        SceneManager.LoadScene(sceneName:"MessengerScene");
+    }
+}
